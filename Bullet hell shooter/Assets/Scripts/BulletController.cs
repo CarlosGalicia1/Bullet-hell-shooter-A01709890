ac17455dfe1d286f85ec4edccc3fd872bdd62573
CarlosGalicia1 <A01709890@tec.mx>
2024-08-20 11:27:49 -0600

    Changed BulletController script

--- Bullet hell shooter/Assets/Scripts/BulletController.cs
+++ Bullet hell shooter/Assets/Scripts/BulletController.cs
@@ -6,16 +6,8 @@ public class BulletController : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (gameObject.layer == LayerMask.NameToLayer("PlayerBullets") && collision.gameObject.layer == LayerMask.NameToLayer("Boss"))
-        {
-            Health health = collision.gameObject.GetComponent<Health>();
-            if (health != null)
-            {
-                health.TakeDamage(damage);
-            }
-            Destroy(gameObject);
-        }
-        else if (gameObject.layer == LayerMask.NameToLayer("BossBullets") && collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if ((gameObject.layer == LayerMask.NameToLayer("PlayerBullets") && collision.gameObject.layer == LayerMask.NameToLayer("Boss")) ||
+            gameObject.layer == LayerMask.NameToLayer("BossBullets") && collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             Health health = collision.gameObject.GetComponent<Health>();
             if (health != null)
